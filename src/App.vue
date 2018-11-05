7228b20e64cb332ba666f4da1ec0bf2be1791f63
onewdivide <onewdivide@hotmail.co.th>
2018-11-05 10:45:05 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+// color Theme
+// orange : #f5a147
+// bluegreen : #51cad8
+// blackblue : #112b39
+// whiteBG : #ffffff
